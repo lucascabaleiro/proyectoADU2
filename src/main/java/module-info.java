898252085665c16dd7d49ab84afb3a9c05f907d3
a21lucascs <cabaleirolucas@gmail.com>
2022-10-06 14:33:42 +0200

    Arreglao

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,6 @@ module com.example.proba {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
