85fc6d0b32932bd81b000eab52646d5b2c8b30b7
a21lucascs <cabaleirolucas@gmail.com>
2022-10-10 10:16:02 +0200

    Tabla funcional

--- src/main/java/com/example/proba/Controller/HelloController.java
+++ src/main/java/com/example/proba/Controller/HelloController.java
@@ -4,6 +4,9 @@ import com.example.proba.Model.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.IOException;
 import java.net.URL;
@@ -11,7 +14,14 @@ import java.net.URL;
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+    @FXML
+    private TableColumn c1;
+    @FXML
+    private TableColumn c2;
+    @FXML
+    private TableColumn c3;
+    @FXML
+    private TableView table1;
     @FXML
     protected void onHelloButtonClick() throws IOException {
         String url = "https://api.jikan.moe/v4/anime/223";
@@ -19,5 +29,10 @@ public class HelloController {
         ObjectMapper objectMapper = new ObjectMapper();
         Response response = objectMapper.readValue(jsonURL, Response.class);
         System.out.println(response.getData().getTitle());
+        c1.setCellValueFactory(new PropertyValueFactory<>("title"));
+        c2.setCellValueFactory(new PropertyValueFactory<>("score"));
+        c3.setCellValueFactory(new PropertyValueFactory<>("popularity"));
+        table1.getItems().add(response.getData());
+
     }
 }
--- src/main/java/com/example/proba/HelloApplication.java
+++ src/main/java/com/example/proba/HelloApplication.java
@@ -3,6 +3,10 @@ package com.example.proba;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,10 +16,29 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("ID anime");
         stage.setScene(scene);
         stage.show();
-        //dsajadsh
+        /*TableView tableView = new TableView();
+        tableView.prefHeightProperty().bind(stage.heightProperty());
+        tableView.prefWidthProperty().bind(stage.widthProperty());
+        TableColumn<String, String> column1 = new TableColumn<>("First Name");
+        column1.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+
+        TableColumn<String, String> column2 = new TableColumn<>("Last Name");
+        column2.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+
+        tableView.getColumns().add(column1);
+        tableView.getColumns().add(column2);
+
+
+        VBox vbox = new VBox(tableView);
+        Scene scene = new Scene(vbox);
+
+        stage.setScene(scene);
+        stage.setTitle("JavaFX TableView Example");
+
+        stage.show();*/
     }
 
     public static void main(String[] args) {
