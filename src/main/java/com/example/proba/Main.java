28577bfe125679aa20f9cf80981dfa7d416c9921
lucascabaleiro <cabaleirolucas@gmail.com>
2022-10-14 20:10:14 +0200

    Main

---
+++ src/main/java/com/example/proba/Main.java
@@ -0,0 +1,9 @@
+package com.example.proba;
+
+import com.example.proba.Controller.HelloController;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+    }
+}
