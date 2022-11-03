deb0f51369a6699dc13d6c95ec5b22ba86ea2667
a21lucascs <cabaleirolucas@gmail.com>
2022-11-03 12:44:08 +0100

    Proba

--- src/main/java/com/example/proba/Main.java
+++ src/main/java/com/example/proba/Main.java
@@ -3,6 +3,7 @@ package com.example.proba;
 import com.example.proba.Controller.HelloController;
 
 public class Main {
+    //Cambios
     public static void main(String[] args) {
         HelloApplication.main(args);
     }
